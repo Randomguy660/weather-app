1f985578d7a3dbea9f3ca88c41a9abb56a1497b8
Lewis Stopford <l.stopford@icloud.com>
2024-02-17 10:06:48 +0000

    added option to find temp of a city

--- src/Components/WeatherApp.tsx
+++ src/Components/WeatherApp.tsx
@@ -1,16 +1,73 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+type getWeatherProps = {
+	lat: number;
+	long: number;
+	cityData?: any;
+};
 
 function WeatherApp() {
 	const [temp, setTemp] = useState<number>();
+	const cityInput = useRef<HTMLInputElement>(null);
+	const [errorMsg, setErrorMsg] = useState<string>("");
+	const fullCityName = useRef<string>();
 
-	function getCoords() {
+	useEffect(() => {
+		cityInput.current ? (cityInput.current.value = "") : undefined;
+	}, [temp]);
+
+	const getCityCoords = async () => {
+		const city = cityInput.current ? cityInput.current.value : "";
+
+		if (city == "") {
+			setErrorMsg("Please enter a city name.");
+			setTemp(undefined);
+		} else {
+			const res = await fetch(
+				`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=153d8e8daf729c54e2ea43c872dc3e5a`
+			);
+			const cityData = await res.json();
+			console.log("city data is ", cityData);
+			if (cityData[0] != undefined) {
+				getWeather({
+					lat: cityData[0].lat,
+					long: cityData[0].lon,
+					cityData: cityData,
+				});
+			} else {
+				setErrorMsg("Invalid city.");
+				setTemp(undefined);
+			}
+		}
+	};
+
+	const getUserCoords = () => {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(
 				(pos) => {
-					getWeather(pos.coords.latitude, pos.coords.longitude);
+					//? Location successfully found
+					const lat = pos.coords.latitude;
+					const long = pos.coords.longitude;
+					getWeather({ lat: lat, long: long });
 				},
-				() => {
-					console.log("Error");
+				(err) => {
+					//? Error in finding location
+					//? err code 1 means user denied access to location
+					//? err code 2 means user's position is unavailable
+					//? err code 3 means the location timed out
+
+					if (err.code === 1) {
+						setErrorMsg(
+							"Please allow access to your location to use this feature."
+						);
+						setTemp(undefined);
+					} else if (err.code === 2) {
+						setErrorMsg("Your location is currently unavailable.");
+						setTemp(undefined);
+					} else {
+						setErrorMsg("Location request timed out.");
+						setTemp(undefined);
+					}
 				},
 				{
 					enableHighAccuracy: false,
@@ -19,22 +76,34 @@ function WeatherApp() {
 				}
 			);
 		} else {
-			console.log("Geolocation is not supported by this browser");
+			setErrorMsg("Your browser does not support location access");
 		}
-	}
+	};
 
-	const getWeather = async (lat: number, long: number) => {
+	const getWeather = async ({ lat, long, cityData }: getWeatherProps) => {
 		const res = await fetch(
 			`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=153d8e8daf729c54e2ea43c872dc3e5a&units=metric`
 		);
 		const weatherData = await res.json();
 		setTemp(weatherData.main.temp);
+		setErrorMsg("Temperature successfully found.");
+		cityData
+			? (fullCityName.current = cityData[0].name)
+			: (fullCityName.current = weatherData.name);
 	};
 
 	return (
 		<>
-			<div>{temp}</div>
-			<button onClick={getCoords}>Get Temp</button>
+			<div>
+				{temp != undefined && temp != null
+					? `The temperature in ${fullCityName.current} is ${temp} degrees Celsius`
+					: "No temperature found."}
+			</div>
+			<button onClick={getCityCoords}>Get Temp</button>
+			<button onClick={getUserCoords}>Get My Temp</button>
+			<br />
+			<input ref={cityInput} type="text" placeholder="Enter City..." />
+			<p>{errorMsg}</p>
 		</>
 	);
 }
